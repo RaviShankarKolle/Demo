170ea5c6791b55730ac59b465f52cdf7631a313f
RaviShankarKolle <164329813+RaviShankarKolle@users.noreply.github.com>
2026-04-24 14:06:45 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import "./styles/Header.css";
+
+const Header = ({searchTerm,setSearchTerm,sortYear,setSortYear,darkMode,setDarkMode}) => {
+   return (<header>
+    <div className="logo">
+        <h1>Coders Movie</h1>
+
+    </div>
+    <div className="header-controls">
+        <input type="text" placeholder="Search movies..." value={searchTerm} 
+        onChange={(e)=>setSearchTerm(e.target.value)}/>
+       
+      
+        <select  value={sortYear || ""} 
+       onChange={(e)=>setSortYear(e.target.value)}>
+            <option value="">Sort By Year</option>
+            <option value="asc">Year Ascending</option>
+            <option value="desc">Year Descending</option>
+
+        </select>
+        <label>
+            <input type="checkbox"
+             checked={darkMode} 
+             onClick={()=>setDarkMode(!darkMode)}/>
+            <span>Dark Mode</span>
+        </label>
+    </div>
+   </header>)
+};
+
+export default Header;
